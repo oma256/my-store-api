0fb4de83401b6bd7a923c9879357550b7a6b8648
Omurbek Dulatov <oma.dulatov@gmail.com>
2023-05-03 22:39:35 +0600

    project architecture

---
+++ users/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils import timezone
+
+from users.managers import UserManager
+from utils.user_phone_number_regex import phone_regex
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    first_name = models.CharField(verbose_name='Имя', max_length=128)
+    last_name = models.CharField(verbose_name='Фамилия', max_length=128)
+    phone_number = models.CharField(verbose_name='Номер телефона',
+                                    validators=[phone_regex],
+                                    max_length=20, unique=True)
+    is_active = models.BooleanField(verbose_name='Активный', default=True)
+    is_staff = models.BooleanField(verbose_name='Сотрудник', default=False)
+    date_joined = models.DateTimeField(verbose_name='Зарегестрирован',
+                                       default=timezone.now)
+
+    USERNAME_FIELD = 'phone_number'
+
+    objects = UserManager()
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return f'{self.phone_number}'
